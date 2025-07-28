feaa51cd559cdc575055bbc03c770d030888dac7
Kumquat <>
2025-07-28 18:48:29 +0200

    Initial setup + fiddling with tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
+
+    // Create component to test
+    fixture = TestBed.createComponent(App);
+    // Access the component through the fixture created by the test framework
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-motocultor');
+  it('should contain any text in a <p> tag', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const anyP = element.querySelector('p')!;
+    expect(anyP.textContent?.length).toBeGreaterThan(0);
   });
 });
