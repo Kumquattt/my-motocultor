f27da57c54d2b99e4427d30d1f33757c96c4b978
Kumquat <>
2025-07-31 22:34:30 +0200

    Refactored storage service

--- src/app/my-motocultor/local-storage-service.ts
+++ src/app/my-motocultor/local-storage-service.ts
@@ -10,35 +10,27 @@ export class LocalStorageService {
   dayKey = 'selectedDay';
 
   getSavedFavoriteSlotsIds(): string[] | null {
-    const savedSlotsJSON = localStorage.getItem(this.favoritesKey);
-    try {
-      if (savedSlotsJSON) {
-        return JSON.parse(savedSlotsJSON) as string[];
-      }
-    } catch (error) {
-      console.error('Issue when fetching slots from local storage: ' + error);
-    }
-    return null;
+    return this.#getContent(this.favoritesKey);
   }
   saveFavoritesSlotsIds(ids: string[]) {
-    localStorage.setItem(this.favoritesKey, JSON.stringify([...new Set(ids)]));
+    this.#setContent(this.favoritesKey, [...new Set(ids)]);
   }
 
   getSavedShowFavoritesToggle(): boolean {
-    return this.#parseContent(this.favedToggleKey) || false;
+    return this.#getContent(this.favedToggleKey) || false;
   }
   saveShowFarovitesToggle(state: boolean) {
-    localStorage.setItem(this.favedToggleKey, JSON.stringify(state));
+    this.#setContent(this.favedToggleKey, state);
   }
 
   getSavedDay(): Day {
-    return this.#parseContent(this.dayKey) as Day;
+    return this.#getContent(this.dayKey) as Day;
   }
   saveDay(day: Day | null) {
     this.#setContent(this.dayKey, day);
   }
 
-  #parseContent(key: string): any {
+  #getContent(key: string): any {
     const content = localStorage.getItem(key);
     if (content) {
       return JSON.parse(content);
